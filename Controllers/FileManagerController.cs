99606de46ccec1b082cc7704b14ff0d92d2ae69a
Brian Thurlow <bthurlow0424@gmail.com>
2019-08-15 13:54:25 -0400

    Support Root Folder Path via ajax.

--- Controllers/FileManagerController.cs
+++ Controllers/FileManagerController.cs
@@ -1,13 +1,10 @@
-﻿using Syncfusion.EJ2.FileManager.AzureFileProvider;
+using Syncfusion.EJ2.FileManager.AzureFileProvider;
 using Microsoft.AspNetCore.Cors;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Newtonsoft.Json;
 using Newtonsoft.Json.Serialization;
-using System.Collections.Generic;
 using Syncfusion.EJ2.FileManager.Base;
-using System;
 
 namespace EJ2FileManagerServices.Controllers
 {
@@ -16,27 +13,42 @@ namespace EJ2FileManagerServices.Controllers
     [EnableCors("AllowAllOrigins")]
     public class FileManagerController : Controller
     {
+        //Set Azure Info Here!
+        private static readonly string baseFolder = "Files";
+        private readonly string startPath = "https://ej2syncfusionfilemanager.blob.core.windows.net/files/";
+        private string originalPath = "https://ej2syncfusionfilemanager.blob.core.windows.net/files/" + baseFolder;
+        private static readonly string accountName = "ej2syncfusionfilemanager";
+        private static readonly string key = "cgKqBPKOGYjPPKn/0eHa9XrYvhPThD43yDAk6QXiEW34kN5cTYY+rD0m/+aHTB1c7TbFSiq3MPDEn8mKMX7jjA==";
+        private static readonly string baseBlobPath = "files";
 
+        //DO NOT MODIFY BELOW HERE
         public string basePath;
         public AzureFileProvider operation;
-    
-        public FileManagerController(IHostingEnvironment hostingEnvironment)
-        {            
+
+        public FileManagerController()//IHostingEnvironment hostingEnvironment
+        {
             this.operation = new AzureFileProvider();
-            this.operation.RegisterAzure("ej2syncfusionfilemanager", "cgKqBPKOGYjPPKn/0eHa9XrYvhPThD43yDAk6QXiEW34kN5cTYY+rD0m/+aHTB1c7TbFSiq3MPDEn8mKMX7jjA==", "files");
-            this.operation.setBlobContainer("https://ej2syncfusionfilemanager.blob.core.windows.net/files/", "https://ej2syncfusionfilemanager.blob.core.windows.net/files/Files");
-            this.operation.setDownloadPath(@"D:\");
+            this.operation.RegisterAzure(accountName, key, baseBlobPath);
+            this.operation.SetBlobContainer(startPath, originalPath);
+            this.operation.SetDownloadPath(@"C:\");
         }
+        [HttpPost]
         [Route("AzureFileOperations")]
-        public object AzureFileOperations([FromBody] FileManagerDirectoryContent args)
+        [Route("AzureFileOperations/{newroot}")]
+        public object AzureFileOperations([FromBody] FileManagerDirectoryContent args, [FromRoute] string newroot)
         {
             if (args.Path != "")
             {
-                var originalPath = "https://ej2syncfusionfilemanager.blob.core.windows.net/files/Files/";
-                string startPath = "https://ej2syncfusionfilemanager.blob.core.windows.net/files/";
                 originalPath = originalPath.Replace(startPath, "");
                 args.Path = (originalPath + args.Path).Replace("//", "/");
                 args.TargetPath = (originalPath + args.TargetPath).Replace("//", "/");
+
+                if (newroot != null)
+                {
+                    string destFolder = args.Path.Replace(baseFolder, "").Replace("/","");
+                    args.Path = (baseFolder + "/" + newroot + "/" + destFolder + "/").Replace("//", "/");
+                    args.TargetPath = (baseFolder + "/" + newroot + "/" + destFolder).Replace("//", "/");
+                }
             }
             switch (args.Action)
             {
@@ -55,8 +67,7 @@ namespace EJ2FileManagerServices.Controllers
                 case "copy":
                     return this.ToCamelCase(this.operation.Copy(args.Path, args.TargetPath, args.Names, args.RenameFiles, args.TargetData, args.Data));
                 case "move":
-                    return this.ToCamelCase(this.operation.Move(args.Path, args.TargetPath, args.Names, args.RenameFiles,args.TargetData, args.Data));
-
+                    return this.ToCamelCase(this.operation.Move(args.Path, args.TargetPath, args.Names, args.RenameFiles, args.TargetData, args.Data));
             }
             return null;
         }
@@ -72,31 +83,49 @@ namespace EJ2FileManagerServices.Controllers
         }
 
         [Route("AzureUpload")]
-        public ActionResult AzureUpload(FileManagerDirectoryContent args)
+        [Route("AzureUpload/{newroot}")]
+        public ActionResult AzureUpload(FileManagerDirectoryContent args, [FromRoute] string newroot)
         {
             if (args.Path != "")
             {
-                var originalPath = "https://ej2filemanager.blob.core.windows.net/files/Files/";
-                string startPath = "https://ej2filemanager.blob.core.windows.net/files/";
                 originalPath = originalPath.Replace(startPath, "");
                 args.Path = (originalPath + args.Path).Replace("//", "/");
+
+                if (newroot != null)
+                {
+                    string destFolder = args.Path.Replace(baseFolder, "").Replace("/", "");
+                    args.Path = (baseFolder + "/" + newroot + "/" + destFolder + "/").Replace("//", "/");
+                    args.TargetPath = (baseFolder + "/" + newroot + "/" + destFolder).Replace("//", "/");
+                }
             }
-            operation.Upload(args.Path, args.UploadFiles,args.Action, args.Data);
+            operation.Upload(args.Path, args.UploadFiles, args.Action, args.Data);
             return Json("");
         }
 
         [Route("AzureDownload")]
-        public object AzureDownload(string downloadInput)
+        [Route("AzureDownload/{newroot}")]
+        public object AzureDownload(string downloadInput, [FromRoute] string newroot)
         {
             FileManagerDirectoryContent args = JsonConvert.DeserializeObject<FileManagerDirectoryContent>(downloadInput);
+            if (newroot != null)
+            {
+                string destFolder = args.Path.Replace(baseFolder, "").Replace("/", "");
+                args.Path = (baseFolder + "/" + newroot + "/" + destFolder + "/").Replace("//", "/");
+                args.TargetPath = (baseFolder + "/" + newroot + "/" + destFolder).Replace("//", "/");
+            }
             return operation.Download(args.Path, args.Names, args.Data);
         }
 
 
         [Route("AzureGetImage")]
-        public IActionResult AzureGetImage(FileManagerDirectoryContent args)
+        [Route("AzureGetImage/{newroot}")]
+        public IActionResult AzureGetImage(FileManagerDirectoryContent args, [FromRoute] string newroot)
         {
-
+            if (newroot != null)
+            {
+                args.Path = "/" + newroot + args.Path.Replace(newroot,"");
+                args.Path = args.Path.Replace("//", "/");
+            }
             return this.operation.GetImage(args.Path, args.Id, true, null, args.Data);
         }
     }
